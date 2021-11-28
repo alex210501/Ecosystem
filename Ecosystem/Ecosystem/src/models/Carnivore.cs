d6489880079489af6bf184c578c37ae81b660f86
alex210501 <alejandrobrb210501@gmail.com>
2021-11-28 17:20:22 +0100

    Implement the Attack() and CanAttack() methods

--- Ecosystem/Ecosystem/src/models/Carnivore.cs
+++ Ecosystem/Ecosystem/src/models/Carnivore.cs
@@ -30,16 +30,15 @@ namespace Ecosystem.src.models
             return false;
         }
 
-        // TODO: Implement the Attack method when the Herbivore is created
         public void Attack(Animal prey)
         {
-            return;
+            if (CanAttack(prey))
+                prey.Health -= damage;
         }
 
-        // TODO: Implement the CanAttack method when the Herbivore is created
         public bool CanAttack(Animal prey)
         {
-            return false;
+            return (prey is Herbivore);
         }
     }
 }
