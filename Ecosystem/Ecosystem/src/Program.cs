c5e6ad9f35984a6d1bc636e93675a5180b844e52
alex210501 <alejandrobrb210501@gmail.com>
2021-11-28 13:26:33 +0100

    Initial commit

---
+++ Ecosystem/Ecosystem/src/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ecosystem
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Ecosystem())
+                game.Run();
+        }
+    }
+}
