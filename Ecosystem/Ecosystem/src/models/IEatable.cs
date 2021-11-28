9ab8dd6c4a1e27189b9b96d1210a7e68b05668c4
alex210501 <alejandrobrb210501@gmail.com>
2021-11-28 15:13:55 +0100

    Add IEatable interface

---
+++ Ecosystem/Ecosystem/src/models/IEatable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ecosystem
+{
+    public interface IEatable
+    {
+        public float EatingEnergy { get; set; }
+        public float SatiationPoint { get; set; }
+    }
+}
