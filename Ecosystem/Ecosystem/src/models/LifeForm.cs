6405e689f9931564802e30dbf852d8bf19db3f8c
alex210501 <alejandrobrb210501@gmail.com>
2021-12-01 20:31:15 +0100

    Changed private to protected threshold values

--- Ecosystem/Ecosystem/src/models/LifeForm.cs
+++ Ecosystem/Ecosystem/src/models/LifeForm.cs
@@ -10,12 +10,12 @@ namespace Ecosystem
     // TODO: Update the diagram for constant variable
     public abstract class LifeForm
     {
-        private static readonly float maxHealth = 100;
-        private static readonly float maxEnergy = 100;
-        private static readonly float maxReproductionDesire = 100;
-        private static readonly float healthToGivePerSecond = 2; // Life to give per second, when we convert life to energy
-        private static readonly float reproductionDesireThreshold = 70;
-        private static readonly float hungerThreshold = 70;
+        protected static readonly float maxHealth = 100;
+        protected static readonly float maxEnergy = 100;
+        protected static readonly float maxReproductionDesire = 100;
+        protected static readonly float healthToGivePerSecond = 2; // Life to give per second, when we convert life to energy
+        protected static readonly float reproductionDesireThreshold = 70;
+        protected static readonly float hungerThreshold = 70;
 
         private float health = 100;
         private float energy = 100;
