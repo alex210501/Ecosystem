2c94cb482f960fe913367be1110ece4d2150e113
alex210501 <alejandrobrb210501@gmail.com>
2021-12-02 16:04:40 +0100

    Implement IEatable in Organic

--- Ecosystem/Ecosystem/src/models/OrganicWaste.cs
+++ Ecosystem/Ecosystem/src/models/OrganicWaste.cs
@@ -4,8 +4,23 @@ using System.Text;
 
 namespace Ecosystem
 {
-    class OrganicWaste : NonLifeForm
+    class OrganicWaste : NonLifeForm, IEatable
     {
+        private float eatingEnergy = 0;
+        private float satiationPoint = 0;
+
         OrganicWaste() : base() { }
+
+        public float EatingEnergy
+        {
+            get { return eatingEnergy; }
+            set { eatingEnergy = value; }
+        }
+
+        public float SatiationPoint
+        {
+            get { return satiationPoint; }
+            set { satiationPoint = value; }
+        }
     }
 }
