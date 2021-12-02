cfb5c98b5672e30336489a1f9f1365f9cd2178c3
alex210501 <alejandrobrb210501@gmail.com>
2021-12-02 16:01:03 +0100

    Implement the IEatable in Meat

--- Ecosystem/Ecosystem/src/models/Meat.cs
+++ Ecosystem/Ecosystem/src/models/Meat.cs
@@ -4,8 +4,23 @@ using System.Text;
 
 namespace Ecosystem
 {
-    public class Meat : NonLifeForm
+    public class Meat : NonLifeForm, IEatable
     {
+        private float eatingEnergy = 0;
+        private float satiationPoint = 0;
+        
         public Meat() : base() { }
+
+        public float EatingEnergy
+        {
+            get { return eatingEnergy; }
+            set { eatingEnergy = value; }
+        }
+
+        public float SatiationPoint
+        {
+            get { return satiationPoint; }
+            set { satiationPoint = value; }
+        }
     }
 }
