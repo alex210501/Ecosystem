a93b3e050a0fa3249c93c3877a7872c72afa2fef
alex210501 <alejandrobrb210501@gmail.com>
2021-12-02 15:43:39 +0100

    Add Meat class

---
+++ Ecosystem/Ecosystem/src/models/Meat.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ecosystem
+{
+    public class Meat : NonLifeForm
+    {
+        public Meat() : base() { }
+    }
+}
